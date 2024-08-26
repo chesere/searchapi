614fc337beca3cc4e19288ef9b4b05d91d2ea5be
chesere <chessm755@gmail.com>
2024-08-26 16:52:18 +0300

    Fetching data from a json api

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -4,6 +4,17 @@ import "./searchBar.css";
 
 export const SearchBar = () => {
   const [input, setInput] = useState("");
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
   return (
     <div className="input-wrap">
       <FaSearch id="search-icon" />
