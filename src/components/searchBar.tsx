707a368700f30b3b99a2ee75488503013c094da0
chesere <chessm755@gmail.com>
2024-08-26 16:40:56 +0300

    Added a state component to store and detect when the input value is changed

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./searchBar.css";
 
 export const SearchBar = () => {
+  const [input, setInput] = useState("");
   return (
     <div className="input-wrap">
       <FaSearch id="search-icon" />
-      <input placeholder="type to search" />
+      <input
+        placeholder="type to search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
   );
 };
