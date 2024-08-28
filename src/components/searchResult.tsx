295085875bf6614351f55bc46b7566ab579885a6
chesere <chessm755@gmail.com>
2024-08-28 14:50:20 +0300

    Added a component to show result

---
+++ src/components/searchResult.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./searchResult.css";
+
+export const SearchResult = ({ result }) => {
+  return (
+    <div
+      className="search-result"
+      onClick={(e) => {
+        alert(`You clicked on ${result.name}`);
+      }}
+    >
+      {result.name}
+    </div>
+  );
+};
