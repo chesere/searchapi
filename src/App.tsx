7ff8455c835e21527d31e80e345db0dc38bc4b87
chesere <chessm755@gmail.com>
2024-08-28 14:21:41 +0300

    Added a new component to show the result list after the filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import { SearchBar } from "./components/searchBar";
+import { SearchResultList } from "./components/searchResultList";
 
 function App() {
+  const [results, setResults] = useState([]);
   return (
     <div className="App">
       <div className="search-bar-container">
-        <SearchBar />
-        <div>SearchDetails</div>
+        <SearchBar setResults={setResults} />
+        <SearchResultList results={results} />
       </div>
     </div>
   );
--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./searchBar.css";
 
-export const SearchBar = () => {
+export const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
   const fetchData = (value) => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((json) => {
         const result = json.filter((user) => {
-          return user && user.name && user.name.toLowerCase().includes(value);
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+          );
         });
-        console.log(result);
+        setResults(result);
       });
   };
   const handleChange = (value) => {
